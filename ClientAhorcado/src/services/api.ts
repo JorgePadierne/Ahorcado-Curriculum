99dbdd5ae7bb1899103ffe878800643c5e4ebb17
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-15 16:01:38 +0100

    a

--- ClientAhorcado/src/services/api.ts
+++ ClientAhorcado/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ahorcado-curriculum.onrender.com", // Cambia esta URL por la de tu backend
+  baseURL: "http://localhost:5032/", // Cambia esta URL por la de tu backend
 });
 
 export default api;
