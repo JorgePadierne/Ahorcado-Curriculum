4acf155fb121ff0c7d65070e2c9b5364bcd607ad
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-16 17:59:34 +0100

    ranking2

--- ClientAhorcado/src/pages/Ranking.tsx
+++ ClientAhorcado/src/pages/Ranking.tsx
@@ -1,28 +1,18 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useAxios } from "../hooks/useAxios";
 function Ranking() {
   const api = useAxios();
-  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
       const { data } = await api.get("/api/juego/puntuaciones");
       console.log(data);
-      setUsers(data);
     };
 
     fetchUsers();
   }, [api]);
 
-  return (
-    <>
-      {users.map((user, index) => (
-        <div key={index}>
-          <h2>{user}</h2>
-        </div>
-      ))}
-    </>
-  );
+  return <></>;
 }
 
 export default Ranking;
