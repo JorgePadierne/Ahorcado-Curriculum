cb0849e9d23ee5c9845037358337d0fdb9fa89db
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-16 18:18:25 +0100

    margin top menos

--- ClientAhorcado/src/pages/Ranking.tsx
+++ ClientAhorcado/src/pages/Ranking.tsx
@@ -21,7 +21,7 @@ function Ranking() {
 
   return (
     <>
-      <div className="max-w-2xl mx-auto mt-10 mt-50">
+      <div className="max-w-2xl mx-auto mt-10 mt-30">
         <h1 className="text-3xl font-extrabold text-center mb-6 text-indigo-700">
           🏆 Ranking
         </h1>
